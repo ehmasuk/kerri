95972bdf9e7802444812ee63df100068f90bf7bb
ehmasuk <ehmasuk@gmail.com>
2022-05-08 22:29:00 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var typed = new Typed('.mac', {
+
+    strings: ['Mashuk.', 'Web Designer.','Web Developer.','Graphic Designer.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 3000,
+    loop: true,
+    smartBackspace: false,
+    });
+
+
+
+
+$(document).ready(function(){
+    var mixer = mixitup("#portfolio_section");
+});
